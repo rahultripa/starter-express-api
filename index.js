b4627cad88eefeaaf7b33099cc2c22c4a20c6c4e
rahul-tripathi-by <64519165+rahul-tripathi-by@users.noreply.github.com>
2023-05-01 18:32:03 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from './state/reducer/index'
+const store=createStore(rootReducer)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ 
+console.warn('Store ',store);
+
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
