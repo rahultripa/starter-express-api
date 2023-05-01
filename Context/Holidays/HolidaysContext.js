12d070138a1f246862e400d8763fd759cad9c668
rahul-tripathi-by <64519165+rahul-tripathi-by@users.noreply.github.com>
2023-05-01 19:26:25 +0530

    Delete

--- Context/Holidays/HolidaysContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const holidaysContext = createContext();
-
-export default holidaysContext;
--- state/constants.js
+++
@@ -1,2 +0,0 @@
-export const ADD_TO_CART="ADD_TO_CART"
-export const SUB_TO_CART="SUB_TO_CART"
